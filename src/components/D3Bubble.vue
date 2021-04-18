29d404284659544f956ca2e3afcf89f6f7a17309
Colin Lee <colinleefish@gmail.com>
2021-04-18 19:26:49 +0800

    PETRIFIED!!!

--- src/components/D3Bubble.vue
+++ src/components/D3Bubble.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3 class="py-24 font-special-elite text-black text-lg text-center">
-      [Here come the bubbles...]
+      [Here come the bubbles...OMG THIS PART IS SUPER HARD LEVEL I'M NOT YET READY FOR THIS]
     </h3>
   </div>
 </template>
